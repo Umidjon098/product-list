4a6477f2c556a2ad22f9cba5535ef1260a127007
Umidjon098 <umidtuychiev098@gmail.com>
2024-05-04 13:59:03 +0500

    suspens added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import ProductList from "./product/product-list";
 import ProfileForm from "./profile-form";
 
 export default function Home() {
   return (
-    <main className="container">
-      <ProductList />
-      <ProfileForm />
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="container">
+        <ProductList />
+        <ProfileForm />
+      </main>
+    </Suspense>
   );
 }
